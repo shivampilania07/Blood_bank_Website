626f32d89ac809ec88fd02d4d090644a5c0fd7bc
Shivam Pilania <pilaniashivam25@gmail.com>
2026-05-21 23:08:38 +0530

    fix express wildcard route render crash

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 // static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
